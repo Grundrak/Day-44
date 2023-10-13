ffa9655eb743cd73d0f09d8656dd82acff8ee25e
Grundrak <electrovilla66@gmail.com>
2023-10-13 15:23:12 +0100

    nnpush

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,41 @@
 const express = require ('express');
 const router = express.Router();
+const Product = require('../models/product');
 
 
+router.get('/', async (req,res) =>{
+    try{
+    const Products = await Product.find();
+    res.json(Products);
+    }catch(err){
+    res.status(500).json({ error: 'An error occurred while fetching data.' });
+    }})
+
+// router.get("/search", async (req, res) => {
+//     const {maxPrice, minPrice, name, availability} = req.query
+  
+//     try {
+//       const query = {};
+  
+//         // if(availability!== undefined) {
+//         //         query.availability = {}
+//         // }
+  
+//       if (name) {
+//         query.name = { $regex: new RegExp(searchquery, "i") };
+//       }else  if (minPrice !== undefined && maxPrice !== undefined) {
+//         query.price = { $gte: minPrice, $lte: maxPrice };
+//       } else if (minPrice !== undefined) {
+//         query.price = { $gte: minPrice };
+//       } else if (maxPrice !== undefined) {
+//         query.price = { $lte: maxPrice };
+//       }
+//       const products = await Product.find(query);
+//       res.render('search', { products });
+//     } catch (err) {
+//       res.status(500).send('Error fetching data from Mngdb');
+//     }
+//   });
+  
 
 module.exports = router ;
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const Product = require('./models/product');
-const connectMongoose = require('./config/connectMng')
+const Port = 4444;
+const router = require('./routes/route');
+require('./config/connectMng');
 
+app.use(express.json());
+app.use(router);
 
-connectMongoose();
-
+app.listen(Port, () => {
+  console.log(`server on port : ${Port}`);
+});
